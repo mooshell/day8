4df827cdf35f2361e7f2576b78396bebb92fc203
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-06-14 11:16:51 -0400

    Initial Commit

---
+++ day8/day8App.swift
@@ -0,0 +1,17 @@
+//
+//  day8App.swift
+//  day8
+//
+//  Created by Michelle Elias Flores on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct day8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
